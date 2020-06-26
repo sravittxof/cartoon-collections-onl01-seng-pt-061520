6be21539b71b9d21386a396e73eb62d7e0e18b3b
Travis Fox <travis.fox90@gmail.com>
2020-06-26 02:11:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,9 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.include
+  
 end
