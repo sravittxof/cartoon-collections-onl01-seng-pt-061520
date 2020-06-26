eab4026f19398d19406c9a33941e6223a5694c7f
Travis Fox <travis.fox90@gmail.com>
2020-06-26 02:04:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index { |name, number|
+    number = number + 1
+    puts "#{number}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize!
+    call << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(planeteer_calls)
+  if pleneteer_calls.find {|i| i.length > 4 }
+  
+  end
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
